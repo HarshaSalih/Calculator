d4ddeba0663ac303c23efd04bdbc67bb4aa40f4f
HarshaSalih <harshasalih2000@gmail.com>
2022-11-21 10:46:28 +0530

    addition commits

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Addition");
+        System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Addition\n5.Other");
+
         System.out.println("Choose an operation");
         Scanner sc=new Scanner(System.in);
         int option=sc.nextInt();
@@ -10,6 +11,11 @@ public class Calculator {
         float num1=sc.nextInt();
         System.out.println("Enter second number");
         float num2=sc.nextInt();
+        switch(option)
+        {
+            case 1:
+                System.out.println("Result is : "+(num1+num2));
+        }
 
 
 
