1c20b4e3e46e33dd001b072120e0ff52c8901fe6
HarshaSalih <harshasalih2000@gmail.com>
2022-11-21 10:36:47 +0530

    commits

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Addition");
+        System.out.println("Choose an operation");
+        Scanner sc=new Scanner(System.in);
+        int option=sc.nextInt();
+        System.out.println("Enter first number");
+        float num1=sc.nextInt();
+        System.out.println("Enter second number");
+        float num2=sc.nextInt();
+
+
+
+
+
+    }
+}
